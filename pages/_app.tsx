412b281c3ebda7320e664612c10f432d96d4f0d7
Chet Woodside <chet@cosmicblend.ca>
2023-11-25 18:25:41 -0800

    add custom button component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,28 @@
-import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
-  arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  base,
   zora,
+  //base,
+  //optimism,
+  //mainnet
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
     zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    //base,
+    //optimism,
+    //miannet
+    //...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
   ],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+  appName: 'rainbowkit_z1155',
   projectId: 'YOUR_PROJECT_ID',
   chains,
 });
