a0d0e1b4a980592be083e4184bc3db30dd2fdcd5
Chet Woodside <chet@cosmicblend.ca>
2023-12-14 13:29:19 -0800

    oops path issue

--- services/premintapi.js
+++ services/premintapi.js
@@ -1,6 +1,6 @@
 const limit = 50;
 
 //export const premintBaseUrl = `https://api.zora.co/discover/premints/ZORA-MAINNET?limit=${limit}`;
-export const premintBaseUrl = `https://your-vercel-project-url.vercel.app/api/zora?limit=${limit}`;
+export const premintBaseUrl = `https://z1155-zora.vercel.app/api/zora?limit=${limit}`;
 
 export const premintFetcher = url => fetch(url).then(res => res.json());
